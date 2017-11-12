799e67658e5fb7e107c98b604474d4484729a467
mocabe <hisa.mocabe@gmail.com>
2017-11-13 02:20:30 +0900

    basic_usage

---
+++ sample/basic_usage.cpp
@@ -0,0 +1,25 @@
+
+#include <nurbs.hpp>
+#include <sample/simple_vector_adaptor.hpp>
+
+using namespace smplv;
+int main(){
+  // 次数
+  size_t degree = 3;
+
+  // 制御点
+  std::vector<dvec4> points = {
+      {2, 5, 0, 1},  {3, 10, 0, 1},  {8, 13, 0, 1}, {10, 4, 0, 0},
+      {20, 7, 0, 0}, {19, 20,0 , 1}, {1, 21, 0, 1}};
+
+  // ノットベクトル
+  auto knots =  nurbs::CreateClampedKnots<double>(points.size(),degree);
+
+  // NURBSクラスの初期化
+  nurbs::NURBS<dvec3, double> nurbs{points, knots, degree};
+
+  // 曲線を100サンプル程度計算する
+  auto result = nurbs.evaluate_all(0.01);
+
+  // 結果を使って何かする
+}
