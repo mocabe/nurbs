dbda0d4473f849dbca7c5ace0aa905517fbf13cd
hisa <hisa.mocabe@gmail.com>
2017-11-16 23:03:37 +0900

    added constexpr

--- sample/glm_adaptor.hpp
+++ sample/glm_adaptor.hpp
@@ -4,32 +4,32 @@
 namespace nurbs {
 using namespace glm;
 template <> struct access<dvec3, 0> {
-  static double &get(dvec3 &v) { return v[0]; }
-  static const double &get(const dvec3 &v) { return v[0]; }
+  static constexpr double &get(dvec3 &v) { return v[0]; }
+  static constexpr const double &get(const dvec3 &v) { return v[0]; }
 };
 template <> struct access<dvec3, 1> {
-  static double &get(dvec3 &v) { return v[1]; }
-  static const double &get(const dvec3 &v) { return v[1]; }
+  static constexpr double &get(dvec3 &v) { return v[1]; }
+  static constexpr const double &get(const dvec3 &v) { return v[1]; }
 };
 template <> struct access<dvec3, 2> {
-  static double &get(dvec3 &v) { return v[2]; }
-  static const double &get(const dvec3 &v) { return v[2]; }
+  static constexpr double &get(dvec3 &v) { return v[2]; }
+  static constexpr const double &get(const dvec3 &v) { return v[2]; }
 };
 template <> struct access<dvec4, 0> {
-  static double &get(dvec4 &v) { return v[0]; }
-  static const double &get(const dvec4 &v) { return v[0]; }
+  static constexpr double &get(dvec4 &v) { return v[0]; }
+  static constexpr const double &get(const dvec4 &v) { return v[0]; }
 };
 template <> struct access<dvec4, 1> {
-  static double &get(dvec4 &v) { return v[1]; }
-  static const double &get(const dvec4 &v) { return v[1]; }
+  static constexpr double &get(dvec4 &v) { return v[1]; }
+  static constexpr const double &get(const dvec4 &v) { return v[1]; }
 };
 template <> struct access<dvec4, 2> {
-  static double &get(dvec4 &v) { return v[2]; }
-  static const double &get(const dvec4 &v) { return v[2]; }
+  static constexpr double &get(dvec4 &v) { return v[2]; }
+  static constexpr const double &get(const dvec4 &v) { return v[2]; }
 };
 template <> struct access<dvec4, 3> {
-  static double &get(dvec4 &v) { return v[3]; }
-  static const double &get(const dvec4 &v) { return v[3]; }
+  static constexpr double &get(dvec4 &v) { return v[3]; }
+  static constexpr const double &get(const dvec4 &v) { return v[3]; }
 };
 
 template <> struct dimension<dvec4> { static constexpr size_t value = 4; };
--- sample/simple_vector_adaptor.hpp
+++ sample/simple_vector_adaptor.hpp
@@ -11,32 +11,32 @@ struct dvec4 {
 namespace nurbs {
   using namespace smplv;
 template <> struct access<dvec3, 0> {
-  static double &get(dvec3 &v) { return v.x; }
-  static const double &get(const dvec3 &v) { return v.x; }
+  static constexpr double &get(dvec3 &v) { return v.x; }
+  static constexpr const double &get(const dvec3 &v) { return v.x; }
 };
 template <> struct access<dvec3, 1> {
-  static double &get(dvec3 &v) { return v.y; }
-  static const double &get(const dvec3 &v) { return v.y; }
+  static constexpr double &get(dvec3 &v) { return v.y; }
+  static constexpr const double &get(const dvec3 &v) { return v.y; }
 };
 template <> struct access<dvec3, 2> {
-  static double &get(dvec3 &v) { return v.z; }
-  static const double &get(const dvec3 &v) { return v.z; }
+  static constexpr double &get(dvec3 &v) { return v.z; }
+  static constexpr const double &get(const dvec3 &v) { return v.z; }
 };
 template <> struct access<dvec4, 0> {
-  static double &get(dvec4 &v) { return v.x; }
-  static const double &get(const dvec4 &v) { return v.x; }
+  static constexpr double &get(dvec4 &v) { return v.x; }
+  static constexpr const double &get(const dvec4 &v) { return v.x; }
 };
 template <> struct access<dvec4, 1> {
-  static double &get(dvec4 &v) { return v.y; }
-  static const double &get(const dvec4 &v) { return v.y; }
+  static constexpr double &get(dvec4 &v) { return v.y; }
+  static constexpr const double &get(const dvec4 &v) { return v.y; }
 };
 template <> struct access<dvec4, 2> {
-  static double &get(dvec4 &v) { return v.z; }
-  static const double &get(const dvec4 &v) { return v.z; }
+  static constexpr double &get(dvec4 &v) { return v.z; }
+  static constexpr const double &get(const dvec4 &v) { return v.z; }
 };
 template <> struct access<dvec4, 3> {
-  static double &get(dvec4 &v) { return v.w; }
-  static const double &get(const dvec4 &v) { return v.w; }
+  static constexpr double &get(dvec4 &v) { return v.w; }
+  static constexpr const double &get(const dvec4 &v) { return v.w; }
 };
 
 template <> struct dimension<dvec4> { static constexpr size_t value = 4; };
