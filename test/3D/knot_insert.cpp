563ddd7a1c259ea21bed8568b5b66ca77e342998
hisa <hisa.mocabe@gmail.com>
2017-11-24 19:46:04 +0900

    flat knot

--- test/3D/knot_insert.cpp
+++ test/3D/knot_insert.cpp
@@ -39,16 +39,7 @@ void ex1(){
     t_assert("inserted knot test", nurbs.knots() == a);
 
     auto r2 = nurbs.evaluate_all(0.125);
-    dvec3 error{0, 0, 0};
-    for(size_t i=0; i< std::min(r1.size(),r2.size()); ++i){
-      error.x = std::max(error.x,std::fabs(r1[i].x - r2[i].x));
-      error.y = std::max(error.y,std::fabs(r1[i].y - r2[i].y));
-      error.z = std::max(error.z,std::fabs(r1[i].z - r2[i].z));
-    }
-    std::cout << "error: ";
-    std::cout << "  {" << error.x << "," << error.y << "," << error.z << "}" << std::endl;
-    double tolerance = 1e-14;
-    t_assert("insert result error test", error.x < tolerance && error.y < tolerance && error.z < tolerance);
+    t_assert("inserted knot test" , r1 == r2);
   }
   { // t = 1
     auto nurbs = NURBS<dvec3, double>(points, knots, degree);
@@ -58,16 +49,7 @@ void ex1(){
     t_assert("inserted knot test", nurbs.knots() == a);
 
     auto r2 = nurbs.evaluate_all(0.125);
-    dvec3 error{0, 0, 0};
-    for(size_t i=0; i< std::min(r1.size(),r2.size()); ++i){
-      error.x = std::max(error.x,std::fabs(r1[i].x - r2[i].x));
-      error.y = std::max(error.y,std::fabs(r1[i].y - r2[i].y));
-      error.z = std::max(error.z,std::fabs(r1[i].z - r2[i].z));
-    }
-    std::cout << "error: ";
-    std::cout << "  {" << error.x << "," << error.y << "," << error.z << "}" << std::endl;
-    double tolerance = 1e-14;
-    t_assert("insert result error test", error.x < tolerance && error.y < tolerance && error.z < tolerance);
+    t_assert("insetred knot test", r1 == r2);
   }
 }
 void weighted_nurbs(){
@@ -112,16 +94,7 @@ void strange_knot(){
     auto r1 = nurbs.evaluate_all(0.125);
     nurbs.knot_insert(1);
     auto r2 = nurbs.evaluate_all(0.125);
-    dvec3 error{0, 0, 0};
-    for(size_t i=0; i< std::min(r1.size(),r2.size()); ++i){
-      error.x = std::max(error.x,std::fabs(r1[i].x - r2[i].x));
-      error.y = std::max(error.y,std::fabs(r1[i].y - r2[i].y));
-      error.z = std::max(error.z,std::fabs(r1[i].z - r2[i].z));
-    }
-    std::cout << "error: ";
-    std::cout << "  {" << error.x << "," << error.y << "," << error.z << "}" << std::endl;
-    double tolerance = 1e-13;
-    t_assert("insert result error test", error.x < tolerance && error.y < tolerance && error.z < tolerance);
+    t_assert("strange knot test1", r1 == r2);
   }
   { 
     std::vector<double> knots(points.size() + degree + 1, 1.);
@@ -130,16 +103,21 @@ void strange_knot(){
     auto r1 = nurbs.evaluate_all(0.125);
     nurbs.knot_insert(1);
     auto r2 = nurbs.evaluate_all(0.125);
-    dvec3 error{0, 0, 0};
-    for(size_t i=0; i< std::min(r1.size(),r2.size()); ++i){
-      error.x = std::max(error.x,std::fabs(r1[i].x - r2[i].x));
-      error.y = std::max(error.y,std::fabs(r1[i].y - r2[i].y));
-      error.z = std::max(error.z,std::fabs(r1[i].z - r2[i].z));
-    }
-    std::cout << "error: ";
-    std::cout << "  {" << error.x << "," << error.y << "," << error.z << "}" << std::endl;
-    double tolerance = 1e-13;
-    t_assert("insert result error test", error.x < tolerance && error.y < tolerance && error.z < tolerance);
+    t_assert("strange knot test2" ,r1 == r2);
+  }
+}
+
+void flat_knot(){
+  size_t degree = 2;
+  {
+    std::vector<dvec4> points = {
+        {1, 1, 1, 1}, {2, 2, 1, 1}, {3, 1, 1, 1}, {4, 1, 1, 1}};
+    std::vector<double> knots(points.size() + degree + 1, 0);
+    NURBS<dvec3, double> nurbs{points, knots, degree};
+    auto r1 = nurbs.evaluate_all(0.25);
+    nurbs.knot_insert(0);
+    auto r2 = nurbs.evaluate_all(0.25);
+    t_assert("flat knot test" ,r1 == r2);
   }
 }
 int main(){
@@ -147,6 +125,7 @@ int main(){
   ex1();
   weighted_nurbs();
   strange_knot();
+  flat_knot();
   test::summarize();
   return test::test_failed;
 }
