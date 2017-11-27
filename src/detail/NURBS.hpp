e3e5974ae234eba95741b099083c7568e764f428
hisa <hisa.mocabe@gmail.com>
2017-11-27 19:38:57 +0900

    result binding

--- src/detail/NURBS.hpp
+++ src/detail/NURBS.hpp
@@ -285,6 +285,21 @@ public:
   template <class EvalTag = tags::default_eval_tag>
   std::vector<point_type> evaluate(const std::pair<knot_type, knot_type> &range,
                                    knot_type interval) const {
+    std::vector<point_type> result;
+    evaluate<EvalTag>(range, interval, result);
+    return result;
+  }
+
+  /**
+   * @fun
+   * @brief evaluate NURBS curve from given t range
+   * @param range evaluate range(max: 0 to 1)
+   * @param interval of each evaluate
+   * @param reference to result vector
+   */
+  template <class EvalTag = tags::default_eval_tag>
+  void evaluate(const std::pair<knot_type, knot_type> &range,
+                knot_type interval, std::vector<point_type> &result) const {
     knot_type tBegin = range.first;
     knot_type tEnd = range.second;
 
@@ -292,28 +307,32 @@ public:
     tBegin = std::clamp(tBegin, knot_type(0), knot_type(1));
     tEnd = std::clamp(tEnd, knot_type(0), knot_type(1));
 
-    if (tBegin > tEnd) std::swap(tBegin,tEnd);
+    if (tBegin > tEnd)
+      std::swap(tBegin, tEnd);
 
     // clamp interval
     interval = std::clamp(interval, knot_type(-1), knot_type(1));
 
+    // clear result
+    result.clear();
+
     // edge cases
-    if (interval == 0) { 
-      std::vector<point_type> ret(2);
-      ret[0] = ret[1] = evaluate<EvalTag>(tBegin);
-      return ret;
-    } else 
-    if (interval == 1) {
-      std::vector<point_type> ret(2);
-      ret[0] = evaluate<EvalTag>(tBegin);
-      ret[1] = evaluate<EvalTag>(tEnd);
-      return ret;
-    } else 
-    if (interval == -1) {
-      std::vector<point_type> ret(2);
-      ret[0] = evaluate<EvalTag>(tEnd);
-      ret[1] = evaluate<EvalTag>(tBegin);
-      return ret;
+    if (interval == 0) {
+      result.reserve(2);
+      result.emplace_back(evaluate<EvalTag>(tBegin));
+      // result[1] = result[0]
+      result.push_back(result.back());
+      return;
+    } else if (interval == 1) {
+      result.reserve(2);
+      result.emplace_back(evaluate<EvalTag>(tBegin));
+      result.emplace_back(evaluate<EvalTag>(tEnd));
+      return;
+    } else if (interval == -1) {
+      result.reserve(2);
+      result.emplace_back(evaluate<EvalTag>(tEnd));
+      result.emplace_back(evaluate<EvalTag>(tBegin));
+      return;
     }
 
     knot_type cBegin{tBegin};
@@ -321,195 +340,209 @@ public:
 
     // negative interval
     if (interval < 0)
-      std::swap(cBegin,cEnd);
+      std::swap(cBegin, cEnd);
 
-
-    // return vector
-    std::vector<point_type> ret((size_t)((cEnd - cBegin) / interval) + 2);
+    // reserve
+    auto sz = ((size_t)((cEnd - cBegin) / interval) + 2);
+    result.reserve(sz);
 
     // eval begin point
-    ret.front() = evaluate<EvalTag>(cBegin);
+    result.emplace_back(evaluate<EvalTag>(cBegin));
 
     std::vector<wpoint_type> buff;
     if constexpr (std::is_same_v<EvalTag, tags::NonRecursive<tags::HeapArray>>)
       buff.resize(degree_ + 1);
 
     // push back each evaluated point
-    for (size_t i = 1; i < ret.size() - 1; ++i)
-      ret[i] = evaluate_impl<EvalTag>(cBegin + i * interval, buff);
+    for (size_t i = 1; i < sz - 1; ++i)
+      result.emplace_back(evaluate_impl<EvalTag>(cBegin + i * interval, buff));
 
     // eval end point
-    ret.back() = evaluate<EvalTag>(cEnd);
+    result.emplace_back(evaluate<EvalTag>(cEnd));
 
-    return ret;
-  }
-
-  /**
-   * @fun
-   * @brief evaluate all area of NURBS curve
-   * calling evaluete({0,1}, interval)
-   * @param interval interval between each evaluation
-   * @return a set of evaluated 3D vectors
-   */
-  template <class EvalTag = tags::default_eval_tag>
-  std::vector<point_type> evaluate_all(double interval) const{
-    return evaluate<EvalTag>({knot_type(0), knot_type(1)}, interval);
-  }
-
-  /**
-   * @fun 
-   * @brief insert knot
-   */
-  NURBS& knot_insert(knot_type); // knot_insert.hpp
-
- private:
-
-  /**
-   * @fun
-   * @brief evaluate impl
-   */
-  template <class EvalTag>
-  point_type evaluate_impl(knot_type t) const{
-    std::vector<wpoint_type> buff;
-    if constexpr (std::is_same_v<EvalTag, tags::NonRecursive<tags::HeapArray>>)
-      std::vector<wpoint_type> buff(degree_ + 1);
-    return evaluate_impl<EvalTag>(t, buff);
-  }
-
-  /**
-   * @fun
-   * @brief evaluate impl
-   */
-  template <class EvalTag>
-  point_type evaluate_impl(knot_type t, std::vector<wpoint_type>& heap_buffer) const {
-
-    // clamp
-    t = std::clamp(t, knot_type(0), knot_type(1));
-
-    auto range = knot_range();
-
-    // convert ratio to actual knot range for evaluate
-    t = range.first + t * (range.second - range.first);
-
-    return knot_evaluate_DeBoor<EvalTag>(t, heap_buffer);
-  }
-
-  /** 
-   * @fun
-   * @brief Calculate De Boor's Algorithm
-   */
-  template <class EvalTag>
-  point_type knot_evaluate_DeBoor(double t, std::vector<wpoint_type>& heap_buffer) const {
-    using namespace tags;
-    using namespace std;
-    size_t index;
-    // find knot span
-    for (index = 0; index < knots_.size() - 2; ++index) {
-      if (knots_[index] <= t && t < knots_[index + 1])
-        break;
+    return;
     }
 
-    while (knots_[index] == knots_[index + 1]) {
-      if(index == 0) break;
-      --index;
+    /**
+     * @fun
+     * @brief evaluate all area of NURBS curve
+     * calling evaluete({0,1}, interval)
+     * @param interval interval between each evaluation
+     * @return a set of evaluated 3D vectors
+     */
+    template <class EvalTag = tags::default_eval_tag>
+    std::vector<point_type> evaluate_all(double interval) const {
+      return evaluate<EvalTag>({knot_type(0), knot_type(1)}, interval);
     }
-    assert(knots_.size() > index);
 
-    // return vector
-    wpoint_type r;
-    if constexpr (is_same_v<EvalTag, NonRecursive<StackArray>> || 
-                  is_same_v<EvalTag, NonRecursive<HeapArray>>) {
-      // buffer
-      wpoint_type* buff;
-      if constexpr (is_same_v<EvalTag, NonRecursive<StackArray>>) {
-        buff = (wpoint_type *)alloca(sizeof(wpoint_type) * (degree_ + 1));
-      } else if constexpr (is_same_v<EvalTag, NonRecursive<HeapArray>>) {
-        heap_buffer.resize(degree_ + 1);
-        buff = heap_buffer.data();
+    /**
+     * @fun
+     * @brief evaluate all area of NURBS curve
+     * calling evaluete({0,1}, interval)
+     * @param interval interval between each evaluation
+     * @param result reference to result
+     */
+    template <class EvalTag = tags::default_eval_tag>
+    void evaluate_all(double interval, std::vector<point_type>& result) const {
+      evaluate<EvalTag>({knot_type(0), knot_type(1)}, interval, result);
+    }
+
+    /**
+     * @fun
+     * @brief insert knot
+     */
+    NURBS &knot_insert(knot_type); // knot_insert.hpp
+
+  private:
+    /**
+     * @fun
+     * @brief evaluate impl
+     */
+    template <class EvalTag> 
+    point_type evaluate_impl(knot_type t) const {
+      std::vector<wpoint_type> buff;
+      return evaluate_impl<EvalTag>(t, buff);
+    }
+
+    /**
+     * @fun
+     * @brief evaluate impl
+     */
+    template <class EvalTag>
+    point_type evaluate_impl(knot_type t, std::vector<wpoint_type> & heap_buffer) const {
+
+      // clamp
+      t = std::clamp(t, knot_type(0), knot_type(1));
+
+      auto range = knot_range();
+
+      // convert ratio to actual knot range for evaluate
+      t = range.first + t * (range.second - range.first);
+
+      return knot_evaluate_DeBoor<EvalTag>(t, heap_buffer);
+    }
+
+    /**
+     * @fun
+     * @brief Calculate De Boor's Algorithm
+     */
+    template <class EvalTag>
+    point_type knot_evaluate_DeBoor(double t, std::vector<wpoint_type> &heap_buffer) const {
+      using namespace tags;
+      using namespace std;
+      size_t index;
+      // find knot span
+      for (index = 0; index < knots_.size() - 2; ++index) {
+        if (knots_[index] <= t && t < knots_[index + 1])
+          break;
       }
-      for (size_t i = 0; i < degree_ + 1; ++i) {
-        // invalid index
-        if ( index + i < degree_ || index - degree_ + i >= points_.size()) {
-          buff[i] = wpoint_type{} * 0;
-        } else {
-          buff[i] = points_[index - degree_ + i];
-          rmult<dimension_v<point_type> - 1>(
-              buff[i], get<dimension_v<point_type>>(buff[i]));
+
+      while (knots_[index] == knots_[index + 1]) {
+        if (index == 0)
+          break;
+        --index;
+      }
+      assert(knots_.size() > index);
+
+      // return vector
+      wpoint_type r;
+      if constexpr (is_same_v<EvalTag, NonRecursive<StackArray>> ||
+                    is_same_v<EvalTag, NonRecursive<HeapArray>>) {
+        // buffer
+        wpoint_type *buff;
+        if constexpr (is_same_v<EvalTag, NonRecursive<StackArray>>) {
+          buff = (wpoint_type *)alloca(sizeof(wpoint_type) * (degree_ + 1));
+        } else if constexpr (is_same_v<EvalTag, NonRecursive<HeapArray>>) {
+          heap_buffer.resize(degree_ + 1);
+          buff = heap_buffer.data();
         }
-      }
-
-      for (size_t i = 0; i < degree_; ++i) {
-        for (size_t j = 0; j < degree_ - i; ++j) {
-          size_t idx = index - degree_ + i + j + 1;
+        for (size_t i = 0; i < degree_ + 1; ++i) {
           // invalid index
-          if ( idx >= knots_.size() || idx + degree_ + 1 - (i + 1) >= knots_.size()) {
-            buff[j] = wpoint_type{} * 0;
+          if (index + i < degree_ || index - degree_ + i >= points_.size()) {
+            buff[i] = wpoint_type{} * 0;
           } else {
-            knot_type d = knots_[idx + degree_ + 1 - (i + 1)] - knots_[idx];
-            knot_type a = (d == 0) ? 0 : (t - knots_[idx]) / d;
-            buff[j] = buff[j] - (buff[j] - buff[j + 1]) * a;
+            buff[i] = points_[index - degree_ + i];
+            rmult<dimension_v<point_type> - 1>(
+                buff[i], get<dimension_v<point_type>>(buff[i]));
           }
         }
-      }
-      r = buff[0];
-    } else if constexpr (is_same_v<EvalTag, Recursive>) {
-      struct {
-        wpoint_type operator()(const NURBS<T, K> &nurbs, knot_type t, size_t i, size_t k) const {
-          if (k == 0) {
+
+        for (size_t i = 0; i < degree_; ++i) {
+          for (size_t j = 0; j < degree_ - i; ++j) {
+            size_t idx = index - degree_ + i + j + 1;
             // invalid index
-            if (i >= nurbs.points_.size())
-              return set<dimension_v<point_type>>(wpoint_type{} * 0,1);
-
-            // convert n degree rational bspine to n+1 degree non-rational
-            // bspline
-            wpoint_type ret = nurbs.points_[i];
-            auto w = get<dimension_v<point_type>>(ret);
-            w = w < 0 ? 0 : w;
-            rmult<dimension_v<point_type> - 1>(ret, w);
-            return ret;
+            if (idx >= knots_.size() ||
+                idx + degree_ + 1 - (i + 1) >= knots_.size()) {
+              buff[j] = wpoint_type{} * 0;
+            } else {
+              knot_type d = knots_[idx + degree_ + 1 - (i + 1)] - knots_[idx];
+              knot_type a = (d == 0) ? 0 : (t - knots_[idx]) / d;
+              buff[j] = buff[j] - (buff[j] - buff[j + 1]) * a;
+            }
           }
-
-          // invalid index
-          if ( i >= nurbs.knots_.size() || i + nurbs.degree_ + 1 - k >= nurbs.knots_.size())
-            return wpoint_type{} * 0;
-
-          knot_type d = nurbs.knots_[i + nurbs.degree_ + 1 - k] - nurbs.knots_[i];
-          knot_type a = (d == 0) ? 0 : (t - nurbs.knots_[i]) / d;
-
-          return ((*this)(nurbs, t, i - 1, k - 1) * (1 - a)) +
-                 ((*this)(nurbs, t, i, k - 1) * a);
         }
-      } calc;
+        r = buff[0];
+      } else if constexpr (is_same_v<EvalTag, Recursive>) {
+        struct {
+          wpoint_type operator()(const NURBS<T, K> &nurbs, knot_type t,
+                                 size_t i, size_t k) const {
+            if (k == 0) {
+              // invalid index
+              if (i >= nurbs.points_.size())
+                return set<dimension_v<point_type>>(wpoint_type{} * 0, 1);
 
-      // evaluate
-      r = calc(*this, t, index, degree_);
-    } else {
-      static_assert(!sizeof(EvalTag), "Invalid Eval Tag");
-    }
-    // Zero weight workaround: estimating possible return point
-    if (get<dimension_v<point_type>>(r) == 0) {
-      size_t bot = (index + 1 < degree_) ? 0 : index + 1 - degree_;
-      // P[index-degree+1] to P[size-1]
-      for (size_t i = bot; i < points_.size(); ++i)
-        if (get<dimension_v<point_type>>(points_[i]) != 0)
-          return degenerate<point_type>(points_[i]);
-      // P[index] back to P[1]
-      for (size_t i = bot; 0 < i; --i)
-        if (get<dimension_v<point_type>>(points_[i]) != 0)
-          return degenerate<point_type>(points_[i]);
-      // P[0]
-      return degenerate<point_type>(points_[0]);
-    } else {
-      rdiv<dimension_v<point_type> - 1>(r, get<dimension_v<point_type>>(r));
-    }
-    // return as point_type
-    return degenerate<point_type>(r);
-  }
+              // convert n degree rational bspine to n+1 degree non-rational
+              // bspline
+              wpoint_type ret = nurbs.points_[i];
+              auto w = get<dimension_v<point_type>>(ret);
+              w = w < 0 ? 0 : w;
+              rmult<dimension_v<point_type> - 1>(ret, w);
+              return ret;
+            }
 
-private:
-  std::vector<wpoint_type> points_;
-  std::vector<knot_type> knots_;
-  size_t degree_;
+            // invalid index
+            if (i >= nurbs.knots_.size() ||
+                i + nurbs.degree_ + 1 - k >= nurbs.knots_.size())
+              return wpoint_type{} * 0;
+
+            knot_type d =
+                nurbs.knots_[i + nurbs.degree_ + 1 - k] - nurbs.knots_[i];
+            knot_type a = (d == 0) ? 0 : (t - nurbs.knots_[i]) / d;
+
+            return ((*this)(nurbs, t, i - 1, k - 1) * (1 - a)) +
+                   ((*this)(nurbs, t, i, k - 1) * a);
+          }
+        } calc;
+
+        // evaluate
+        r = calc(*this, t, index, degree_);
+      } else {
+        static_assert(!sizeof(EvalTag), "Invalid Eval Tag");
+      }
+      // Zero weight workaround: estimating possible return point
+      if (get<dimension_v<point_type>>(r) == 0) {
+        size_t bot = (index + 1 < degree_) ? 0 : index + 1 - degree_;
+        // P[index-degree+1] to P[size-1]
+        for (size_t i = bot; i < points_.size(); ++i)
+          if (get<dimension_v<point_type>>(points_[i]) != 0)
+            return degenerate<point_type>(points_[i]);
+        // P[index] back to P[1]
+        for (size_t i = bot; 0 < i; --i)
+          if (get<dimension_v<point_type>>(points_[i]) != 0)
+            return degenerate<point_type>(points_[i]);
+        // P[0]
+        return degenerate<point_type>(points_[0]);
+      } else {
+        rdiv<dimension_v<point_type> - 1>(r, get<dimension_v<point_type>>(r));
+      }
+      // return as point_type
+      return degenerate<point_type>(r);
+    }
+
+  private:
+    std::vector<wpoint_type> points_;
+    std::vector<knot_type> knots_;
+    size_t degree_;
 };
 
 }
