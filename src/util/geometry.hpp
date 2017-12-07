92d07c75d1bcbce96abaf3abe42c478857b0f593
hisa <hisa.mocabe@gmail.com>
2017-12-08 00:03:10 +0900

    constexpr

--- src/util/geometry.hpp
+++ src/util/geometry.hpp
@@ -6,7 +6,7 @@ namespace nurbs {
 namespace impl{
 template <class V, size_t N> 
 struct dot_impl {
-  static auto dot(const V &lhs, const V &rhs) {
+  static constexpr auto dot(const V &lhs, const V &rhs) {
     return get<N - 1>(lhs) * get<N - 1>(rhs) +
            dot_impl<V, N - 1>::dot(lhs, rhs);
   }
@@ -14,7 +14,7 @@ struct dot_impl {
 
 template <class V> 
 struct dot_impl<V, 1> {
-  static auto dot(const V &lhs, const V &rhs) {
+  static constexpr auto dot(const V &lhs, const V &rhs) {
     return get<0>(lhs) * get<0>(rhs);
   }
 };
@@ -24,14 +24,14 @@ struct dot_impl<V, 0> {};
 }
 
 template <class V>
-auto dot(const V& lhs, const V& rhs){
+constexpr auto dot(const V& lhs, const V& rhs){
   return impl::dot_impl<V, dimension_v<V>>::dot(lhs, rhs);
 }
 
 
 template <class V, class = std::enable_if_t<dimension_v<V> == 3>>
-V cross(const V& lhs, const V& rhs) {
-  V v;
+constexpr V cross(const V& lhs, const V& rhs) {
+  V v {};
   get<0>(v) =  get<1>(lhs) * get<2>(rhs) - get<2>(lhs) * get<1>(rhs);
   get<1>(v) =  get<2>(lhs) * get<0>(rhs) - get<0>(lhs) * get<2>(rhs);
   get<2>(v) =  get<0>(lhs) * get<1>(rhs) - get<1>(lhs) * get<0>(rhs);
@@ -40,13 +40,13 @@ V cross(const V& lhs, const V& rhs) {
 
 
 template <class V>
-auto distance(const V& lhs, const V& rhs) {
+constexpr auto distance(const V& lhs, const V& rhs) {
   auto v = rhs - lhs;
   return std::sqrt(dot(v, v));
 };
 
 template <class V>
-V normalize(const V& v) {
+constexpr V normalize(const V& v) {
   auto div = std::sqrt(dot(v, v));
   if (div == 0)
     return v;
