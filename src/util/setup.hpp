c0cda6bd22fb27c6304c0463a99d0735136ee131
hisa <hisa.mocabe@gmail.com>
2018-01-07 21:08:08 +0900

    is_debug

--- src/util/setup.hpp
+++ src/util/setup.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#if defined(NDEBUG)
+constexpr bool is_debug = false;
+#else
+#include <iostream>
+constexpr bool is_debug = true;
+#endif
+
 #include "dimension.hpp"
 #include "access.hpp"
 #include "vector_traits.hpp"
