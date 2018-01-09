58a361f24e4a02b266128b23aaf795bb75ee7196
hisa <hisa.mocabe@gmail.com>
2018-01-09 14:16:43 +0900

    added debug_msg_level

--- src/util/setup.hpp
+++ src/util/setup.hpp
@@ -2,9 +2,15 @@
 
 #if defined(NDEBUG)
 constexpr bool is_debug = false;
+constexpr unsigned debug_msg_level = 0;
 #else
 #include <iostream>
 constexpr bool is_debug = true;
+#if defined(NURBS_DEBUG_MSG_LEVEL)
+constexpr unsigned debug_msg_level = NURBS_DEBUG_MSG_LEVEL;
+#else
+constexpr unsigned debug_msg_level = 1;
+#endif
 #endif
 
 #include "dimension.hpp"
